c3c101465c979f86b9d47e841dac9b8daa42ca91
Nicolás Lagos <n.lagos.sepulveda@outlook.com>
2025-01-24 10:23:27 -0300

    Se crean las funcionalidades del Challenge

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Lista con nombres de amigos
+let listaDeAmigos = [];
+
+//Función para agregar amigos
+function agregarAmigo() {
+    let amigo = (document.getElementById("amigo").value).trim();
+
+    if (amigo.length === 0) {
+        alert("Por favor, inserte un nombre");
+        document.getElementById("amigo").value = '';
+
+        return;
+    }
+
+    listaDeAmigos.push(amigo);
+    document.getElementById("amigo").value = '';
+    mostrarAmigos();
+
+    return;
+}
+
+//Función para actualizar lista de amigos
+function mostrarAmigos() {
+    let listaMostrada = document.getElementById("listaAmigos");
+    listaMostrada.innerHTML = ""
+
+    for (amigo of listaDeAmigos) {
+        let elementoDeLista = document.createElement("li");
+        elementoDeLista.append(amigo);
+        listaMostrada.append(elementoDeLista);
+    }
+
+    return;
+}
+
+//Función para sortear el amigo secreto
+function sortearAmigo() {
+    if (listaDeAmigos.length === 0) {
+        return;
+    }
+
+    let indice = Math.floor(Math.random()*listaDeAmigos.length);
+    let nombreSorteado = listaDeAmigos[indice];
+    document.getElementById("resultado").innerHTML = nombreSorteado;
+
+    return;
+}
